4710dbc771ed22ed7ff00b8178e4b7237c8c970a
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-01-09 12:22:10 -0700

    Add files via upload

--- Day_9_Secret_auction/day_9_secret_auction.py
+++ Day_9_Secret_auction/day_9_secret_auction.py
@@ -1,3 +1,16 @@
+import os
+
+def find_highest_bider(bidders):
+    highest_bid = 0
+    highest_bidder = ""
+    for bidder in bidders:
+        amount = bidders[bidder]
+        if amount > highest_bid:
+            highest_bid = amount
+            highest_bidder = bidder
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+
+
 game_on = False
 bidders = {}
 while not game_on:
@@ -7,13 +20,8 @@ while not game_on:
     more_bidders = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
     if more_bidders == 'no':
         game_on = True
+        find_highest_bider(bidders)
+    elif more_bidders == 'yes':
+        os.system('cls')
 
-highest_bid = 0
-highest_bidder = ""
-for bidder in bidders:
-    amount = bidders[bidder]
-    if amount > highest_bid:
-        highest_bid = amount
-        highest_bidder = bidder
 
-print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
