4ea162e7c0cffd6e7aea3db865b29e4d9c663a11
Salvador Montiel <126306642+PeterPan2151@users.noreply.github.com>
2025-01-09 12:14:51 -0700

    Add files via upload
    Secret auction program using dictionaries

---
+++ Day_9_Secret_auction/day_9_secret_auction.py
@@ -0,0 +1,19 @@
+game_on = False
+bidders = {}
+while not game_on:
+    name = input("What is your name? ")
+    bid_amount = float(input("What's your bid? "))
+    bidders[name] = bid_amount
+    more_bidders = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    if more_bidders == 'no':
+        game_on = True
+
+highest_bid = 0
+highest_bidder = ""
+for bidder in bidders:
+    amount = bidders[bidder]
+    if amount > highest_bid:
+        highest_bid = amount
+        highest_bidder = bidder
+
+print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
